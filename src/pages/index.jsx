578c0fe0772148c5c3df7f4da936231c99d1754b
liurixing <liurixing@bytedance.com>
2021-09-15 16:46:11 +0800

    feat: 使用useEventListener管理监听

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useRef, useState } from 'react';
 import ROUTES from '@/config/routes';
 import { HashRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import { arr2json } from '@/common/utils/index.js';
+import useEventListener from '@/common/hooks/useEventListener.js';
 import './index.scss';
 
 export default () => {
+  
   const routeJSON = useRef(arr2json(ROUTES, 'route'));
-
+  
   const getTitle = () => {
     const { route, title = route.slice(1) } = routeJSON.current?.[location.hash.slice(1)];
     document.title = title;
   };
+  
+  useEventListener('hashchange', getTitle);
 
-  useEffect(() => {
-    getTitle();
-    window.onhashchange = getTitle;
-  }, []);
+  useEffect(getTitle, []);
 
   return (
     <Router>

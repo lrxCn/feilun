6b15e0dd5775a26644c17c211c0b38bcdfe55c79
liurixing <liurixing@bytedance.com>
2021-09-15 18:56:32 +0800

    feat: 打包使用相对路径

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
